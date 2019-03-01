232f4236501bd1314c4e37d79ee95772e4abcc58
Christian Wirén <christian.wiren@me.com>
2019-03-01 15:57:28 +0100

    Revert "mergad kod"
    This reverts commit aa8e6f834a23ca9c0284e1a9a1136485f4c9378a, reversing
    changes made to 2f46e3fbe7995ff165ae6c36c3ed2ba29ba8705c.

--- public/js/script.js
+++ public/js/script.js
@@ -2,28 +2,16 @@ function menu() {
     document.querySelector('.menu').classList.toggle('active');
 }
 
-
-var pagesCustomer = new Vue({
+var pages = new Vue({
     el: '#pages',
     data: {
-        index: 0,
-        count: 0,
-        track: 0,
+        index: 0
     },
     methods: {
-        nextButton: function(index) {
-          this.index++;
-           window.scrollTo(0,0);
-        },
-        nextButtonCompany: function(count){
-        this.count++;
-          window.scrollTo(0,0);
-        },
-        nextButtonTrack: function(track){
-          this.track++;
-          window.scrollTo(0,0);
+        nextButton: function() {
+            this.index++;
+            window.scrollTo(0,0);
         }
-      }
-
-    });
+    }
+});
 
